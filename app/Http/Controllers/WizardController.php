d734e3fc883fb23ea6cb00e0eb926fe4c25b0451
Carlos <carlos012100@gmail.com>
2025-03-24 20:58:37 +0100

    [UPDATE]

--- app/Http/Controllers/WizardController.php
+++ app/Http/Controllers/WizardController.php
@@ -22,7 +22,7 @@ class WizardController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'age' => 'required|string|min:0',
+            'age' => 'required|string|min:1',
             'magic_level' => 'required|integer|min:1',
         ]);
 
